9dce0a26c1f3f8870ae9ab787fee21abe697b6d4
Luís Lopes <luis.lopes@mindera.com>
2022-07-01 17:05:40 +0100

    Update generator with OpenAPI Generator 6.0.0 changes

---
+++ src/main/java/com/mindera/openapi/generator/Swift5ClientCodegen.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ * Copyright 2018 SmartBear Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mindera.openapi.generator;
+
+public class Swift5ClientCodegen extends org.openapitools.codegen.languages.Swift5ClientCodegen {
+
+    public Swift5ClientCodegen() {
+        super();
+
+        this.typeMapping.put("date", "DateWithoutTime");
+        this.typeMapping.put("Date", "DateWithoutTime");
+        this.typeMapping.put("DateTime", "DateWithTime");
+        this.typeMapping.put("DateWithTime", "Date");
+    }
+
+    @Override
+    public String getName() {
+        return "mindera-swift5";
+    }
+}
